2dbfe80734f1cfa98cca8ea8e5046cdaee680e2a
Alexis Ibarra <aibarra@viajanet.com.ve>
2013-07-03 18:30:54 -0430

    Cambios para culminar el punto 5.12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,19 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+	 
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 end
 
 	private
